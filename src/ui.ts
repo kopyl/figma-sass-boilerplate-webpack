a8738c5134fb98443efa4d06aa5f68cd6c94e78b
kopyl <oleggggg1997@gmail.com>
2022-01-20 16:58:44 +0200

    first commit

---
+++ src/ui.ts
@@ -0,0 +1,11 @@
+import "./ui.sass"
+
+document.getElementById('create').onclick = () => {
+    const textbox = document.getElementById('count') as HTMLInputElement
+    const count = parseInt(textbox.value, 10)
+    parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*')
+  }
+  
+  document.getElementById('cancel').onclick = () => {
+    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
+  }
